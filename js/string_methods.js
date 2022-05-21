efd52e62a8800c1a11e32fc91d790f2bbd236c95
devbreezemar <brissa.hernandez2021@gmail.com>
2022-05-20 20:18:26 -0500

    add changes

--- js/string_methods.js
+++ js/string_methods.js
@@ -29,8 +29,31 @@ window.alert(converter)*/
 
 //The search() Method
 
-let frase = 'Por que en los ojos correctos siempre seras arte'
+//let frase = 'Por que en los ojos correctos siempre seras arte'
 
-let buscar = frase.search('ojos')
-window.alert(frase.concat(' --->  =', buscar))
-    //console.log(buscar)
+//let buscar = frase.search('ojos')
+//window.alert(frase.concat(' --->  =', buscar))
+//console.log(buscar)
+
+
+//'Hola Koders'
+//Cuantas vocales existen
+
+let vocales = prompt('Ingresa una palabra')
+
+if (vocales === null) {
+    console.log('No has ingresado nada')
+} else {
+    let cuentaVocal = vocales.match(/[aeiou]/g).length + 'vocales'
+    alert(`La palabra ${vocales} tiene ${cuentaVocal} vocales`);
+}
+
+/**
+ * 'Hola Koders'
+ * Cuantos caracteres son
+ */
+
+
+//'Hola Koders'
+//Cortar el string en 2 strings 
+//-> "hola" "koders"
